333e2d2d47625d603cde52681bcb1aab38e9a905
Kyle Luke <th.oldschool@gmail.com>
2019-11-05 03:09:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -42,7 +42,8 @@ class Application
 
   def add_item(item)
     if @@items.include?(item)
-      @@cart << "#{item}"
+      @@cart << item
+      return "added #{item}"
     else
       resp.write "We don't have that item"
     end
