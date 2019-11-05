c78c4d606d809a386b2a6d3b13ca6aad15d1ea5c
Kyle Luke <th.oldschool@gmail.com>
2019-11-05 02:58:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
       if !@@cart.length == 0
         @@cart.each do |item|
           resp.write "#{item}\n"
+        end
       else
         resp.write "Your cart is empty"
       end
